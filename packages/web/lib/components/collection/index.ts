9ae10b34ae2812d9127fda88b42238343cef4079
thxforall <113906780+thxforall@users.noreply.github.com>
2026-03-27 00:45:04 +0900

    chore(50-01): remove mock data from collectionStore and delete dead collection components
    - Delete MOCK_PINS and MOCK_BOARDS constants from collectionStore.ts
    - Make loadCollection a no-op (mock data removed)
    - Delete PinGrid, PinCard, BoardGrid, BoardCard, CollageView, CollectionHeader
    - Update collection/index.ts to remove deleted component exports

--- packages/web/lib/components/collection/index.ts
+++ packages/web/lib/components/collection/index.ts
@@ -1,7 +1,2 @@
-// Pinterest-style collection components (used in Profile > Saved tab)
-export { PinGrid } from "./PinGrid";
-export { PinCard } from "./PinCard";
-export { BoardGrid } from "./BoardGrid";
-export { BoardCard } from "./BoardCard";
-export { CollageView } from "./CollageView";
+// Collection components (magazine/bookshelf/studio)
 export { CollectionShareSheet } from "./CollectionShareSheet";
